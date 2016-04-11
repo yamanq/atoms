daaa7e9d36a582bbed3c6f0d4421dc3df4866e84
ksjdragon <ksjdragon@gmail.com>
2016-04-11 00:35:31 -0400

    minor display fixes

--- index.js
+++ index.js
@@ -327,10 +327,10 @@ function getAtomDOM(atomNum, size) {
 get("pulltab")[0].onclick = function(){open(get("elements"));}
 get("pulltab")[1].onclick = function(){open(get("settings"));}
 get("fa")[0].onclick = function(){
-	get("elements").style.marginLeft = "-70%";
+	get("elements").style.marginLeft = "-150%";
 }
 get("fa")[1].onclick = function(){
-	get("settings").style.marginLeft = "-20%";
+	get("settings").style.marginLeft = "-40%";
 }
 
 function open(dom) {
